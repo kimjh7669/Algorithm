7d7c6a452519e3beeb89792a16bdad58b3b8ad4e
kimjh7669 <55736215+kimjh7669@users.noreply.github.com>
2021-06-25 15:30:36 +0900

    Create 4963_섬의 개수.py
    https://www.acmicpc.net/problem/4963
    
    문제
    정사각형으로 이루어져 있는 섬과 바다 지도가 주어진다. 섬의 개수를 세는 프로그램을 작성하시오.
    
    
    
    한 정사각형과 가로, 세로 또는 대각선으로 연결되어 있는 사각형은 걸어갈 수 있는 사각형이다. 
    
    두 정사각형이 같은 섬에 있으려면, 한 정사각형에서 다른 정사각형으로 걸어서 갈 수 있는 경로가 있어야 한다. 지도는 바다로 둘러싸여 있으며, 지도 밖으로 나갈 수 없다.
    
    입력
    입력은 여러 개의 테스트 케이스로 이루어져 있다. 각 테스트 케이스의 첫째 줄에는 지도의 너비 w와 높이 h가 주어진다. w와 h는 50보다 작거나 같은 양의 정수이다.
    
    둘째 줄부터 h개 줄에는 지도가 주어진다. 1은 땅, 0은 바다이다.
    
    입력의 마지막 줄에는 0이 두 개 주어진다.
    
    출력
    각 테스트 케이스에 대해서, 섬의 개수를 출력한다.

---
+++ Beakjun/4963_섬의 개수.py
@@ -0,0 +1,84 @@
+import sys
+from collections import deque
+
+w, h = map(int, input().split())
+
+dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+
+def dfs(i, j):
+        q = deque()
+        q.append([i,j])
+        graph[i][j] = 0
+        while q:
+                a, b = q.popleft()
+                for k in range(8):
+                        x = a + dx[k]
+                        y = b + dy[k]
+                        if 0 <= x < h and 0 <= y < w and graph[x][y] == 1:
+                                graph[x][y] = 0
+                                q.append([x, y])
+                                
+                                
+
+                        
+
+
+while w!=0 and h!=0:
+       
+        graph = []
+        for i in range(h):
+                graph.append(list(map(int, input().split())))
+        count = 0
+        for i in range(h):
+                for j in range(w):
+                        if graph[i][j] == 1:
+                                dfs(i, j)
+                                count += 1
+        print(count)
+        w, h = map(int, input().split())
+        
+        
+        
+        
+        
+        
+# import sys
+# from collections import deque
+
+# w, h = map(int, input().split())
+
+# dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+# dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+
+# def bfs(i, j):
+#         q = deque()
+#         q.append([i,j])
+#         graph[i][j] = 0
+#         while q:
+#                 a, b = q.popleft()
+#                 for k in range(8):
+#                         x = a + dx[k]
+#                         y = b + dy[k]
+#                         if 0 <= x < h and 0 <= y < w and graph[x][y] == 1:
+#                                 graph[x][y] = 0
+#                                 q.append([x, y])
+                                
+                                
+
+                        
+
+
+# while w!=0 and h!=0:
+       
+#         graph = []
+#         for i in range(h):
+#                 graph.append(list(map(int, input().split())))
+#         count = 0
+#         for i in range(h):
+#                 for j in range(w):
+#                         if graph[i][j] == 1:
+#                                 bfs(i, j)
+#                                 count += 1
+#         print(count)
+#         w, h = map(int, input().split())
