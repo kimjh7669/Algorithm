4543a3c97b6d1600e21314feaefc542469bb5a96
kimjh7669 <55736215+kimjh7669@users.noreply.github.com>
2021-06-25 16:46:43 +0900

    Update 1991_트리 순회.py
    https://www.acmicpc.net/problem/1991
    
    트리 순회 성공
    시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율
    2 초	128 MB	24676	15416	11753	63.833%
    문제
    이진 트리를 입력받아 전위 순회(preorder traversal), 중위 순회(inorder traversal), 후위 순회(postorder traversal)한 결과를 출력하는 프로그램을 작성하시오.
    
    
    
    예를 들어 위와 같은 이진 트리가 입력되면,
    
    전위 순회한 결과 : ABDCEFG // (루트) (왼쪽 자식) (오른쪽 자식)
    중위 순회한 결과 : DBAECFG // (왼쪽 자식) (루트) (오른쪽 자식)
    후위 순회한 결과 : DBEGFCA // (왼쪽 자식) (오른쪽 자식) (루트)
    가 된다.
    
    입력
    첫째 줄에는 이진 트리의 노드의 개수 N(1≤N≤26)이 주어진다. 둘째 줄부터 N개의 줄에 걸쳐 각 노드와 그의 왼쪽 자식 노드, 오른쪽 자식 노드가 주어진다. 노드의 이름은 A부터 차례대로 영문자 대문자로 매겨지며, 항상 A가 루트 노드가 된다. 자식 노드가 없는 경우에는 .으로 표현된다.
    
    출력
    첫째 줄에 전위 순회, 둘째 줄에 중위 순회, 셋째 줄에 후위 순회한 결과를 출력한다. 각 줄에 N개의 알파벳을 공백 없이 출력하면 된다.

--- Beakjun/1991_트리 순회.py
+++ Beakjun/1991_트리 순회.py
@@ -1,89 +1,33 @@
-import sys
-from collections import deque
+def preorder(node):
+        if node =='.':
+                return
+        print(node, end='')
+        preorder(tree[node][0])
+        preorder(tree[node][1])
 
-N = int(input())
+def inorder(node):
+        if node =='.':
+                return
+        inorder(tree[node][0])
+        print(node,end='')
+        inorder(tree[node][1])
 
-graph = []
+def postorder(node):
+        if node =='.':
+                return
+        postorder(tree[node][0])
+        postorder(tree[node][1])
+        print(node, end='')
 
-for i in range(N):
-        graph.append(list(sys.stdin.readline().strip().split(' ')))
+n = int(input())
+tree = {}
+# maketree
+for _ in range(n):
+        root,left,right = input().split()
+        tree[root] = (left, right)
 
-# 전위 순회한 결과
-# [0][1][2] 순서로 queue에 넣는다.
-
-queue = []
-visit = [0] * N
-visit[0] = 1
-
-queue.append(graph[0][2])
-queue.append(graph[0][1])
-queue.append(graph[0][0])
-
-answer = ""
-while queue:
-        a = queue.pop()
-        answer = answer + a
-        num = ord(a) - 65
-        if visit[num] == 0:
-                for i in range(2):
-                        if graph[num][i+1] != '.':
-                                queue.append(graph[num][i+1])                                        
-                visit[num] = 1
-print(answer)
-
-
-
-# 중위 순회한 결과
-# [1][0][2] 순서로 stack에 넣는다.
-
-queue = deque()
-visit = [0] * N
-visit[0] = 1
-
-queue.append(graph[0][2])
-queue.append(graph[0][0])
-queue.append(graph[0][1])
-
-answer = ""
-while queue:
-        a = queue.pop()
-        # answer = answer + a
-        num = ord(a) - 65
-        if visit[num] == 0:
-                queue.append(a)
-                if graph[num][2] != '.':
-                        queue.appendleft(graph[num][2])
-                if graph[num][1] != '.':
-                        queue.append(graph[num][1])
-                visit[num] = 1
-        else:
-                answer = answer + a
-print(answer)
-
-
-# 후위 순회한 결과
-# [2][1][0] 순서로 stack에 넣는다.
-# print(graph)
-
-queue = deque()
-visit = [0] * N
-visit[0] = 1
-
-queue.append(graph[0][0])
-queue.append(graph[0][2])
-queue.append(graph[0][1])
-
-answer = ""
-while queue:
-        a = queue.pop()
-        num = ord(a) - 65
-        if visit[num] == 0:
-                queue.append(a)
-                if graph[num][2] != '.':
-                        queue.append(graph[num][2])
-                if graph[num][1] != '.':
-                        queue.append(graph[num][1])
-                visit[num] = 1
-        else:
-                answer = answer + a
-print(answer)
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
