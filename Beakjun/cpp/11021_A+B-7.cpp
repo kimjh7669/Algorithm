01bb56dbfbe7f85d90c247b2da7ccfaeb5cec14f
kimjh7669 <55736215+kimjh7669@users.noreply.github.com>
2021-06-30 14:13:57 +0900

    Create 11021_A+B-7.cpp

---
+++ Beakjun/cpp/11021_A+B-7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i ++ )
+        {
+		int a, b;
+		scanf("%d %d", &a, &b);
+		cout << "Case #" << i << ": " << a+b << "\n";
+	}
+	return 0;
+}
