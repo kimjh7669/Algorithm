1f363efa76eb322e27fb013282757097c1856328
kimjh7669 <55736215+kimjh7669@users.noreply.github.com>
2021-06-30 14:20:06 +0900

    Create 11718_그대로 출력하기.cpp

---
+++ Beakjun/cpp/11718_그대로 출력하기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void){
+	string str;
+        while(getline(cin, str)){
+                cout << str << endl;
+        }
+        
+
+	return 0;
+}
