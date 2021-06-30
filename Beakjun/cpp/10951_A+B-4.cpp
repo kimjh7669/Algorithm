e9776fe3532cc90ed87629594a6ac45692c9afd4
kimjh7669 <55736215+kimjh7669@users.noreply.github.com>
2021-06-30 14:06:29 +0900

    Create 10951_A+B-4.cpp

---
+++ Beakjun/cpp/10951_A+B-4.cpp
@@ -0,0 +1,22 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(int argc, char** argv)
+// {
+//         int num1, num2;
+//         while(scanf("%d %d", &num1, &num2) != EOF)
+//                 printf("%d\n", num1 + num2);
+//         return 0;
+// }
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+        while(cin >> a >> b)
+                cout << a + b << endl;
+        return 0;
+}
