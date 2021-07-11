47ba6698469c7933d466e13a088a405426c6c185
kimjh7669 <55736215+kimjh7669@users.noreply.github.com>
2021-07-11 13:04:36 +0900

    Create 약수의 개수와 덧셈.cpp

---
+++ Programmers/Lv1/약수의 개수와 덧셈.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int left, int right) {
+    int answer = 0;
+    for(int i = left; i<=right ; i++){
+        int temp = 0;
+        for(int j = 1; j <=right ; j++){
+            if(i%j == 0){
+                temp ++;
+            }
+        }
+        if(temp %2 == 0) answer += i;
+        else answer -= i;
+    }
+    return answer;
+}
