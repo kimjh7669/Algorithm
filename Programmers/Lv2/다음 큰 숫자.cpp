87f631526c54fc0a2303606e56ca049dbc5ec521
kimjh7669 <55736215+kimjh7669@users.noreply.github.com>
2021-07-13 11:59:57 +0900

    Update 다음 큰 숫자.cpp

--- Programmers/Lv2/다음 큰 숫자.cpp
+++ Programmers/Lv2/다음 큰 숫자.cpp
@@ -1,3 +1,38 @@
+#include <string>
+#include <vector>
+using namespace std;
+string to_2(int N){
+    string A = "";
+    string temp;
+    while(N >0){
+        temp = (char)(N%2 + 48);
+        N /= 2;
+        A = temp+A;
+    }
+    return A;
+}
+
+int count1(string A){
+    int ret = 0;
+    for(int i = 0; i<A.size(); i++){
+        if(A[i] == '1') ret ++;
+    }
+    return ret;
+}
+
+int solution(int n) {
+    int answer = 0;
+    int num_1 = count1(to_2(n));
+    for(int i = n+1;;i++){
+        if(num_1 == count1(to_2(i))){
+            answer = i;
+            break;
+        }
+    }
+    
+    return answer;
+}
+
 
 // 시간 초과 내가 푼 것.
 
