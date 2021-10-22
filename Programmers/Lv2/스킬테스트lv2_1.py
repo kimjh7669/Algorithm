b423a18167f53f71a0ea55399d1d7f1575965d5e
kimjh7669 <55736215+kimjh7669@users.noreply.github.com>
2021-10-22 13:10:46 +0900

    Update 스킬테스트lv2_1.py

--- Programmers/Lv2/스킬테스트lv2_1.py
+++ Programmers/Lv2/스킬테스트lv2_1.py
@@ -10,14 +10,11 @@ def solution(prior, location):
         level[prior[i]] += 1
     
     num = 0
-    temp = 0
     while len(lst) != 0:
         cur_num = lst.pop(0)    
         cur_prior = dic[cur_num]
         if_append = False
-        for i in range(cur_prior+1, 10, 1):
-            temp += 1
-            
+        for i in range(cur_prior+1, 10, 1):            
             if level[i] != 0:
                 lst.append(cur_num)
                 if_append = True
@@ -27,7 +24,5 @@ def solution(prior, location):
             level[dic[cur_num]] -= 1
             if cur_num == location:
                 return num
-        if temp > 1000:
-            break
-            
+
     return answer
