155c14349ca478dde468eae2fb43f814339c7665
kimjh7669 <55736215+kimjh7669@users.noreply.github.com>
2022-01-07 17:25:03 +0900

    Create 섬 연결하기.py

---
+++ Programmers/Lv3/섬 연결하기.py
@@ -0,0 +1,15 @@
+def solution(n, costs):
+    # kruskal algorithm
+    ans = 0
+    costs.sort(key = lambda x: x[2]) # cost 기준으로 오름차순 정렬
+    routes = set([costs[0][0]]) # 집합
+    while len(routes)!=n:
+        for i, cost in enumerate(costs):
+            if cost[0] in routes and cost[1] in routes:
+                continue
+            if cost[0] in routes or cost[1] in routes:
+                routes.update([cost[0], cost[1]])
+                ans += cost[2]
+                costs[i] = [-1, -1, -1]
+                break
+    return ans
